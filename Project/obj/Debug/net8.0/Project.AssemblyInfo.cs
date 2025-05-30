666a45796083cd91a63a8ef826270a9ac9348f04
TyLandry <tylerclandry22@gmail.com>
2025-05-30 10:02:15 -0600

    line endings update

--- Project/obj/Debug/net8.0/Project.AssemblyInfo.cs
+++ Project/obj/Debug/net8.0/Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4caa98433e13bf4c940af785c46d30f4bccdcc4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
